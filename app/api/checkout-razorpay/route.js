c79299a2e231ad578859357081f77f3b2e197cf6
vivekcbanakar-ui <vivekcbanakar-ui@users.noreply.github.com>
2026-09-18 04:33:38 +0000

    Fix auth import path; add zod

--- app/api/checkout-razorpay/route.js
+++ app/api/checkout-razorpay/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "./auth/[...nextauth]/route";
+import { authOptions } from "./[...nextauth]/route";
 import Razorpay from "razorpay";
 import { z } from "zod";
 
